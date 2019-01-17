eaf4485d8421acd60c355de96ca6121e4ff2cd53
Juxte <xaviergs1984@gmail.com>
2019-01-17 21:00:31 +0100

    Added Fibonacci test files

--- tests/test_fib.py
+++ tests/test_fib.py
@@ -1,11 +1,5 @@
-import pytest
-from fib import fibR
+# test_fib.py
+import fib
 
-def test_fib_1_equals_1():
-    assert fibR(1) == 1
-
-def test_fib_2_equals_1():
-    assert fibR(2) == 1
-
-def test_fib_6_equals_8():
-    assert fibR(6) == 8
+def test_fib():
+    assert fib.fib(0) == 1

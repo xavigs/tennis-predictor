cf0281e04ec919c76fb503eb7e54d2599c7e3e98
Juxte <xaviergs1984@gmail.com>
2019-01-22 20:57:06 +0100

    Added Brendan replace

--- lib/utils.py
+++ lib/utils.py
@@ -91,7 +91,7 @@ def searchKeyDictionaryFromTE(matrix, key, value):
                     return index
 
             # 5th case
-            replace_dict = {"Julien": "Julian", "Marco": "Marko", "Brinkman": "Brinkmann", "Flavius": "flavius", "Samuel": "Sam"}
+            replace_dict = {"Julien": "Julian", "Marco": "Marko", "Brinkman": "Brinkmann", "Flavius": "flavius", "Samuel": "Sam", "Brendan": "Brendon"}
             new_value = value
 
             for orig_string, new_string in replace_dict.items():

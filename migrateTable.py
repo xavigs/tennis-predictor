779afac610d8eb603def038d6c0aad6a3f8f9357
Juxte <xaviergs1984@gmail.com>
2019-03-14 21:59:25 +0100

    Table migration

--- migrateTable.py
+++ migrateTable.py
@@ -18,14 +18,16 @@ new_rows = []
 cluster = Cluster(["127.0.0.1"])
 session = cluster.connect("beast")
 
-query = "SELECT * FROM player"
+'''
+query = "SELECT * FROM " + orig_table
 rows = session.execute(query)
 
 for row in rows:
-    query = "INSERT INTO player (player_C_career, player_C_season, player_C_year, player_G_career, player_G_season, player_G_year, player_H_career, player_H_season, player_H_year, player_I_career, player_I_season, player_I_year, player_birth, player_rankmax, player_te_name, player_te_url, player_atpwt_id, player_country, player_name, player_race, player_ranking, player_rankdate, player_keyword) VALUES (-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, '1900-01-01', -1, 'BLANK', 'BLANK', '" + row.player_atpwt_id + "', '" + row.player_country + "', '" + row.player_name + "', " + str(row.player_race) + ", " + str(row.player_ranking) + ", '" + str(row.player_rankdate) + "', '" + row.player_keyword + "')"
+    query = "INSERT INTO " + target_table + " (player_C_career, player_C_season, player_C_year, player_G_career, player_G_season, player_G_year, player_H_career, player_H_season, player_H_year, player_I_career, player_I_season, player_I_year, player_birth, player_rankmax, player_te_name, player_te_url, player_atpwt_id, player_country, player_name, player_race, player_ranking, player_rankdate, player_keyword) VALUES (-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, '1900-01-01', -1, 'BLANK', 'BLANK', '" + row.player_atpwt_id + "', '" + row.player_country + "', '" + row.player_name + "', " + str(row.player_race) + ", " + str(row.player_ranking) + ", '" + str(row.player_rankdate) + "', '" + row.player_keyword + "')"
     print(query)
     session.execute(query)
 exit()
+'''
 
 # Get columns from the target table
 query = "SELECT * FROM system_schema.columns WHERE keyspace_name = 'beast' AND table_name = '" + target_table + "'"
@@ -34,7 +36,12 @@ schema_columns = session.execute(query)
 for schema_column in schema_columns:
     column = dict()
     column['name'] = schema_column.column_name
-    column_names.append(column['name'])
+
+    if "_" in column['name']:
+        column_names.append("\"" + column['name'] + "\"")
+    else:
+        column_names.append(column['name'])
+
     column['type'] = schema_column.type
     columns.append(column)
 
@@ -47,11 +54,22 @@ for row in rows:
     row = list(row)
     print(row)
     insert = "INSERT INTO " + target_table + " (" + separator.join(column_names) + ") VALUES ("
-    '''
+    index = 0
+    values = []
+
     for column in columns:
-        if column['type'] == "text" or column['type'] == date:
-            insert += "'" + row. + "'"
-    '''
+        row[index] = str(row[index]).replace("'", "''")
+
+        if column['type'] == "text" or column['type'] == "date":
+            values.append("'" + row[index] + "'")
+        else:
+            values.append(row[index])
+
+        index += 1
+
+    insert += separator.join(values) + ")"
+    print(insert)
+    session.execute(insert)
 
 # Close connections
 session.shutdown()

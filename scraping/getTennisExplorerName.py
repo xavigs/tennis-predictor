305c7b70979c4350de41fb15591f2387a074ceb9
Juxte <xaviergs1984@gmail.com>
2019-04-03 22:44:56 +0200

    Update Mexican players

--- scraping/getTennisExplorerName.py
+++ scraping/getTennisExplorerName.py
@@ -47,7 +47,7 @@ countries = soup.select("tbody#rank-country td a")
 
 for index, country in enumerate(countries):
     # Test from specific country
-    if country.text.strip() and index == 43:
+    if country.text.strip() and index == 46:
         country_pycountry = pycountry.countries.get(name=country.text.strip())
 
         if country_pycountry is None:
@@ -88,7 +88,7 @@ for index, country in enumerate(countries):
                         te_name = list(player.select("td"))[1].text.strip().split(", ")
                         atp_id = utils.searchKeyDictionaryByValue(players_db, "name", te_name[1] + " " + te_name[0], True)
 
-                        if atp_id and te_name[1] + " " + te_name[0] != "Eyal Cohen":
+                        if atp_id and te_name[1] + " " + te_name[0] != "Agustin Moreno":
                             print("Jugador localitzat: " + te_name[1] + " " + te_name[0] + "!!! (" + players_db[atp_id]['rankdate'] + ") - " + list(player.select("a"))[0]['href'])
                             print(atp_id)
 

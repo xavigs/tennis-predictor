33fcb80dc901bb9613c18bc6819aced8502323e5
Juxte <xaviergs1984@gmail.com>
2019-04-04 09:08:51 +0200

    Update New Zealander players

--- scraping/getTennisExplorerName.py
+++ scraping/getTennisExplorerName.py
@@ -47,7 +47,7 @@ countries = soup.select("tbody#rank-country td a")
 
 for index, country in enumerate(countries):
     # Test from specific country
-    if country.text.strip() and index == 61:
+    if country.text.strip() and index == 64:
         country_pycountry = pycountry.countries.get(name=country.text.strip())
 
         if country_pycountry is None:
